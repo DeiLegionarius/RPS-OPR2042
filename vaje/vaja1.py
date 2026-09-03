b366017f3496ff123bdb2fcfa414bd0e9a3b71a7
DeiLegionarius <152004340+DeiLegionarius@users.noreply.github.com>
2026-09-03 06:13:44 +0000

    novi commit

--- vaje/vaja1.py
+++ vaje/vaja1.py
@@ -1,1 +1,16 @@
-print("hello world")
+import time
+
+def timedPrint(data, end="\n"):
+    txtData = data[0::2]
+    etData = data[1::2]
+    for i in range(len(txtData)):
+        elapsedT = etData[i]
+        txt = txtData[i]
+        perNum = elapsedT / len(txt)
+        for letter in txt:
+            print(letter, end="", flush=True)
+            time.sleep(perNum)
+    print(end)
+        
+        
+timedPrint(("test", 1))
