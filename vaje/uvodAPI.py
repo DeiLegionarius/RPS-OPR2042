82e1429788af41beb5751bc1af4e749543c2353d
DeiLegionarius <152004340+DeiLegionarius@users.noreply.github.com>
2026-09-10 06:51:37 +0000

    neavem

--- vaje/uvodAPI.py
+++ vaje/uvodAPI.py
@@ -1,5 +1,12 @@
 import requests
+import json
 
-base_url = "https://api.chucknorris.io/jokes/random"
+openmeteo = "https://api.open-meteo.com/v1/forecast?latitude=46.2389&longitude=14.3556&daily=sunrise,sunset&current=temperature_2m,rain&timezone=Europe%2FBerlin&forecast_days=1"
+nominatim = "https://nominatim.openstreetmap.org/search?city=kranj&format=jsonv2"
 
-call = requests.get
+
+call = requests.get(nominatim)
+callJSON = call.json()
+
+for key in callJSON.keys():
+    print(f"{key}: {callJSON[key]}")
