e3a5d80aaba6dc718f21c2168d1b50443ed01044
DeiLegionarius <152004340+DeiLegionarius@users.noreply.github.com>
2026-09-14 11:00:11 +0000

    komit

--- vaje/uvodAPI.py
+++ vaje/uvodAPI.py
@@ -1,12 +1,22 @@
 import requests
 import json
 
-openmeteo = "https://api.open-meteo.com/v1/forecast?latitude=46.2389&longitude=14.3556&daily=sunrise,sunset&current=temperature_2m,rain&timezone=Europe%2FBerlin&forecast_days=1"
-nominatim = "https://nominatim.openstreetmap.org/search?city=kranj&format=jsonv2"
+openmeteo = "https://api.open-meteo.com/v1/forecast?latitude=46.2389&daily=temperature_2m_mean&longitude=14.3556&daily=sunrise,sunset&current=temperature_2m,rain&timezone=Europe%2FBerlin&forecast_days=7"
 
 
-call = requests.get(nominatim)
+call = requests.get(openmeteo)
 callJSON = call.json()
 
-for key in callJSON.keys():
-    print(f"{key}: {callJSON[key]}")
+if False:
+    for key in callJSON.keys():
+        print(f"{key}: {callJSON[key]}")
+        
+print(f"trenutna temperatura: {callJSON['current']['temperature_2m']}")
+print(f"napovedana temperatura: {callJSON['daily']['temperature_2m_mean']}")
+
+temperature = callJSON['daily']['temperature_2m_mean']
+maxtemp = max(temperature)
+maxtempDatum = callJSON['daily']['time'][temperature.index(maxtemp)]
+print((maxtemp, maxtempDatum))
+
+
