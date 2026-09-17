df555ce7d8453b13ef9765e80a449fa285033c00
DeiLegionarius <152004340+DeiLegionarius@users.noreply.github.com>
2026-09-17 06:45:25 +0000

    dodan openmeteovaja

---
+++ vaje/openmeteovaja.py
@@ -0,0 +1,73 @@
+import requests
+
+def trenutna_temp2(lat, lon):
+    global base_url
+    params = {"latitude": lat,
+              "longitude": lon,
+              "current": "temperature_2m",
+              "timezone": "auto",
+              "forecast_days": 1
+              }
+    call = requests.get(base_url, params=params)
+    callurl = call.url
+    calljson = call.json()
+    print(callurl)
+    
+def getForecast(url, lat, lon, days):
+    params = {
+        "latitude":lat,
+        "longitude":lon,
+        "forecast_days":days,
+        "daily":"temperature_2m_mean,sunrise,sunset,rain"
+    }
+    call = requests.get(url, params=params)
+    print(call.url)
+    return call.json()
+    
+def getCityData(url, name, count):
+    params = {
+        "name": name,
+        "count": count,
+        "language":"en",
+        "format": "json"
+    }
+    call = requests.get(url, params=params)
+    if count == 1 or len(call.json["results"])==1:
+        return call.json()["results"][0], False
+    return call.json()["results"], True
+
+def betterInput(text:str, requiredtype:type, default=None):
+    inp = None
+    while type(inp) != requiredtype:
+        inp = input(text)
+        if not default is None and inp=="":
+            inp = default
+            break
+    return requiredtype(inp)
+            
+
+def main():
+    forecast_url = "https://api.open-meteo.com/v1/forecast"
+    geocoding_url = "https://geocoding-api.open-meteo.com/v1/search"
+    city = betterInput("Enter city name: ", str)
+    options = betterInput("How many options (press ENTER for default): ", int, 1)
+    citydata = getCityData(geocoding_url, city, options)
+    if citydata[1]:
+        citydata = citydata[0]
+        print("MULTIPLE POSSIBLE RESULTS FOUND!")
+        for item in citydata:
+            print(f"{citydata.index(item)+1}: {item["name"]}, {item["country"]} (pop: {item["population"]}, timezone: {item["timezone"]})")
+        selection = betterInput("Select the city you wanted (press ENTER for default): ", int, 1)
+        citydata = citydata[selection]
+    else:
+        citydata = citydata[0]
+    forecastDays = betterInput("For how many days should the forecast be?(press ENTER for default): ", int, 7)
+    forecast = getForecast(forecast_url, citydata["latitude"], citydata["longitude"], forecastDays)
+    print(f"-------- FORECAST FOR {citydata["name"].upper()}, {citydata["country"].upper()} --------")
+    for item in forecast:
+        print(f"{item}: {forecast[item]}")
+    
+    
+    
+if __name__ == "__main__":
+    main()
--- vaje/uvodAPI.py
+++ vaje/uvodAPI.py
@@ -1,22 +1,28 @@
 import requests
 import json
 
-openmeteo = "https://api.open-meteo.com/v1/forecast?latitude=46.2389&daily=temperature_2m_mean&longitude=14.3556&daily=sunrise,sunset&current=temperature_2m,rain&timezone=Europe%2FBerlin&forecast_days=7"
+openmeteo = "https://api.open-meteo.com/v1/forecast?latitude=46.2389&daily=temperature_2m_mean,sunrise,sunset&longitude=14.3556&current=temperature_2m,rain&timezone=Europe%2FBerlin&forecast_days=7"
+nominatim = "https://nominatim.openstreetmap.org/search?city=Kranj&country=Slovenia"
+params = {"city": "Kranj",
+          "country": "Slovenia"}
+headers = {
+    "User-Agent": "School_API_Practice/1.0 (joj.sitecen@gmail.com)"
+}
 
-
-call = requests.get(openmeteo)
+call = requests.get(nominatim)
 callJSON = call.json()
 
 if False:
     for key in callJSON.keys():
         print(f"{key}: {callJSON[key]}")
         
-print(f"trenutna temperatura: {callJSON['current']['temperature_2m']}")
-print(f"napovedana temperatura: {callJSON['daily']['temperature_2m_mean']}")
+if False:
+    print(f"trenutna temperatura: {callJSON['current']['temperature_2m']}")
+    print(f"napovedana temperatura: {callJSON['daily']['temperature_2m_mean']}")
 
-temperature = callJSON['daily']['temperature_2m_mean']
-maxtemp = max(temperature)
-maxtempDatum = callJSON['daily']['time'][temperature.index(maxtemp)]
-print((maxtemp, maxtempDatum))
+    temperature = callJSON['daily']['temperature_2m_mean']
+    maxtemp = max(temperature)
+    maxtempDatum = callJSON['daily']['time'][temperature.index(maxtemp)]
+    print((maxtemp, maxtempDatum))
 
 
